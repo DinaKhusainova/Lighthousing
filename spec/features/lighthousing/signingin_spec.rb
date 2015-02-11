6e7b4fa661e19ffcfb8c834fb1443a22869c85ca
DinaKhusainova <Dinachusainova@seznam.cz>
2015-02-11 19:10:15 +0100

    Edited signing test

--- spec/features/lighthousing/signingin_spec.rb
+++ spec/features/lighthousing/signingin_spec.rb
@@ -5,29 +5,31 @@ require 'spec_helper'
 
 describe "Signing in"do
     before do
-    Admin.create(email:"admin1@gmail.com", password: "heslo123")
+        Admin.create(email:"admin1@gmail.com", password: "heslo123")
     end
     
     it "Signing in as an admin successfully"do
-    visit "/admins/sign_in"
-    expect(page).to have_content("Log in")
+        visit "/admins/sign_in"
+        expect(page).to have_content("Log in")
         
-    fill_in "Email", with: "admin1@gmail.com"
-    fill_in "Password", with: "heslo123"
-    check "Remember me"
-    click_button "Log in"
-    expect(page).to have_text("Log out")
+        fill_in "Email", with: "admin1@gmail.com"
+        fill_in "Password", with: "heslo123"
+        check "Remember me"
+        click_button "Log in"
+        expect(page).to have_text("Log out")
     
-    visit "/"
-    click_link "Be a model (TFP)"
+        visit "/"
+        click_link "Be a model (TFP)"
     
         
-    fill_in "* Name", with: "Mandy"
-    fill_in "* Email", with: "mandy@gmail.com"
-    fill_in "* Message", with: "Hello!"
-    click_button "Send request"
+        fill_in "* Name", with: "Mandy"
+        fill_in "* Email", with: "mandy@gmail.com"
+        fill_in "* Message", with: "Hello!"
+        click_button "Send request"
         
-    click_link "Admin"
-    expect(page).to have_text("Mandy")
+        click_link "Admin"
+        within "table"do
+            expect("Mandy")
+        end
     end
 end 
